9de75d7a097d395fbf5fa72903591f84b2338cf5
Min Soo Kim <mskim@softmagic.com>
2015-02-12 18:33:44 +0900

    add puts when inserting to app.files for rubymotion

--- lib/rlayout.rb
+++ lib/rlayout.rb
@@ -6,6 +6,7 @@ if RUBY_ENGINE == "rubymotion"
 
   Motion::Project::App.setup do |app|
     Dir.glob(File.join(File.dirname(__FILE__), 'rlayout/**/*.rb')).each do |file|
+      puts "files included:#{file}"
       app.files.unshift(file)
     end
   end
