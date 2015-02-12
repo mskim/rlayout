40fcd00a370a9938e0fd89f778879ab3115b4c31
Min Soo Kim <mskim@softmagic.com>
2015-02-12 18:41:17 +0900

    trying rubymotion setting

--- lib/rlayout.rb
+++ lib/rlayout.rb
@@ -1,5 +1,5 @@
-if RUBY_ENGINE == "rubymotion"
-  puts "rubymotion "
+# if RUBY_ENGINE == "rubymotion"
+#   puts "rubymotion "
   unless defined?(Motion::Project::Config)
     raise "This file must be required within a RubyMotion project Rakefile."
   end
@@ -10,29 +10,29 @@ if RUBY_ENGINE == "rubymotion"
       app.files.unshift(file)
     end
   end
-else
-  if  RUBY_ENGINE == 'macruby'
-    puts "MacRuby"
-  # elsif RUBY_ENGINE == 'ruby' 
-    require File.dirname(__FILE__) + "/rlayout/version"
-    require File.dirname(__FILE__) + "/rlayout/utility"
-    require File.dirname(__FILE__) + "/rlayout/graphic"
-    require File.dirname(__FILE__) + "/rlayout/container"
-    require File.dirname(__FILE__) + "/rlayout/object_box"
-    require File.dirname(__FILE__) + "/rlayout/text_column"
-    require File.dirname(__FILE__) + "/rlayout/text_box"
-    require File.dirname(__FILE__) + "/rlayout/text"
-    require File.dirname(__FILE__) + "/rlayout/page"
-    require File.dirname(__FILE__) + "/rlayout/page_variables_extend"
-    require File.dirname(__FILE__) + "/rlayout/document_variables_extend"
-    require File.dirname(__FILE__) + "/rlayout/view"
-    require File.dirname(__FILE__) + "/rlayout/document"
-    require File.dirname(__FILE__) + "/rlayout/article"
-    require File.dirname(__FILE__) + "/rlayout/publication/newspaper"
-    require File.dirname(__FILE__) + "/rlayout/publication/photo_book"
-  end
-end
-
-module RLayout
-  # Your code goes here...
-end
+# else
+#   if  RUBY_ENGINE == 'macruby'
+#     puts "MacRuby"
+#   # elsif RUBY_ENGINE == 'ruby' 
+#     require File.dirname(__FILE__) + "/rlayout/version"
+#     require File.dirname(__FILE__) + "/rlayout/utility"
+#     require File.dirname(__FILE__) + "/rlayout/graphic"
+#     require File.dirname(__FILE__) + "/rlayout/container"
+#     require File.dirname(__FILE__) + "/rlayout/object_box"
+#     require File.dirname(__FILE__) + "/rlayout/text_column"
+#     require File.dirname(__FILE__) + "/rlayout/text_box"
+#     require File.dirname(__FILE__) + "/rlayout/text"
+#     require File.dirname(__FILE__) + "/rlayout/page"
+#     require File.dirname(__FILE__) + "/rlayout/page_variables_extend"
+#     require File.dirname(__FILE__) + "/rlayout/document_variables_extend"
+#     require File.dirname(__FILE__) + "/rlayout/view"
+#     require File.dirname(__FILE__) + "/rlayout/document"
+#     require File.dirname(__FILE__) + "/rlayout/article"
+#     require File.dirname(__FILE__) + "/rlayout/publication/newspaper"
+#     require File.dirname(__FILE__) + "/rlayout/publication/photo_book"
+#   end
+# end
+# 
+# module RLayout
+#   # Your code goes here...
+# end
