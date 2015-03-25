9abf0c2b29c5700b4faaf668833d9b7e56173b77
Min Soo Kim <mskim@softmagic.com>
2015-03-25 14:22:43 +0900

    add cli utility md2story

--- lib/rlayout/article/story.rb
+++ lib/rlayout/article/story.rb
@@ -75,79 +75,14 @@ module RLayout
         File.open(path, 'w'){|f| f.write to_hash.to_yaml}
       end
     end
-    
-    def self.parse_markdown(source, options={})
-      @para_data=[]
-      return @para_data if source == " " || source == ""
-      demotion_level = options.fetch(:demotion_level, 0).to_i
-       
-      Kramdown::Document.new(source).root.children.each do |child|
-        unless child.type == :blank
-          para={}
-          # puts "child.type:#{child.inspect}"
-          case child.type
-          when :p
-            # puts "child.child.class:#{child.child.class}" 
-            para[:markup] = "p" 
-            para[:string] = child.children[0].value # inner text
-            if child.children[0].type == :img
-              para[:markup] = "img" 
-              # puts "child.children[0].attr:#{child.children[0].attr}"
-              para[:image_path] = child.children[0].attr['src']
-              para[:caption] = child.children[0].attr['alt']
-              @para_data << para
-            else
-              @para_data << para
-            end
-          when :header
-            # puts "child.options[:level].class:#{child.options[:level].class}"
-            # demotion_level = child.options[:level] + demotion_level
-            # puts "demotion_level:#{demotion_level}"
-            # if demotion_level  > 6
-            #   demotion_level = 6
-            # end
-            level = child.options[:level] + demotion_level
-            level = 6 if level > 6
-            para[:markup] = "h#{level}"
-            para[:string] = child.options[:raw_text] # inner text
-            @para_data << para
-            
-          when "hr"
-            para[:markup] = "h#{child.attrubute.level}"
-            para << child.value
-            @para_data << para
-
-          else
-            # para[:markup] = child.value # markup
-            # para[:string] = child.value # inner text
-            # @para_data << para
-          end
-
-        end
-      end
-      @para_data
-    end
-    
+        
     def self.from_meta_markdown(filename)
       unless File.exists?(filename)
         puts "Can not find file #{filename}!!!!"
         return 
       end
-      
-      contents = File.read(filename)
-      begin
-        if (md = contents.match(/^(---\s*\n.*?\n?)^(---\s*$\n?)/m))
-          @contents = md.post_match
-          @metadata = YAML.load(md.to_s)          
-        end
-      rescue => e
-        puts "YAML Exception reading #filename: #{e.message}"
-      end  
-      # convert all keys to symbol
-      @metadata = @metadata[0] if @metadata.class == Array
-      @metadata=Hash[@metadata.map{ |k, v| [k.to_sym, v] }]
-      demotion_level = @metadata.fetch(:demote, 0).to_i
-      Story.new(:heading=>@metadata, :paragraphs=>Story.parse_markdown(@contents, :demotion_level=>demotion_level))
+      a = `md2story "#{target}"`
+      Story.new(eval(a))
     end
     
     def self.sample(options={})
@@ -168,7 +103,6 @@ module RLayout
         :paragraphs => ParagraphModel.body_part(5)
       }
       Story.new(h)
-      
     end    
 
     def self.news_article_sample(options={})
