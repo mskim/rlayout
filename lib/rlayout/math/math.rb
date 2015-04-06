dba16470f795ff2f12c0a1735e8403e51f130af6
Min Soo Kim <mskim@softmagic.com>
2015-04-06 11:19:31 +0900

    add math.rb

---
+++ lib/rlayout/math/math.rb
@@ -0,0 +1,198 @@
+module RLayout
+
+  # This is when we multiple MRows
+  class MathBox < Container
+    attr_accessor :asciimath, :hash_tree,
+
+    def initialize(parent_graphic, options={})
+      @direction = 'horizontal'
+      @asciimath = options[:asciimath]
+
+      #TODO read mrows
+      #TODO How is multipe rows represented? in Asciimath
+      toke_hash  = Asciimath.parse(asciimath).parsed_tokens
+      mrows.each do |row_ascii_math|
+        MRow.new(self, asciimath=> row_ascii_math)
+      end
+      self
+    end
+
+
+  end
+
+  class MRow < Container
+
+    attr_accessor :asciimath, :hash_tree,
+
+    def initialize(parent_graphic, options={})
+      super
+      @direction = 'horizontal'
+      if options[:asciimath]
+        @asciimath = options[:asciimath]
+        toke_hash  = Asciimath.parse(asciimath).parsed_tokens
+        toke_hash.each do |token_hash|
+          MRow.create_token(parent, token_hash)
+        end
+      elsif options[:toke_hash]
+        MRow.create_token(parent, token_hash)
+      end
+      self
+    end
+
+    def self.create_token(parent, token_hash)
+      case token_hash[:type]
+      when :operator
+        Moperator.new(parent, token_hash[:c])
+      when :identifier
+        Midentifier.new(parent, token_hash[:c])
+      when :number
+        MNumber.new(parent, token_hash[:c])
+      when :text
+        MText.new(parent, token_hash[:c])
+      when :paren
+        paren = !opts[:strip_paren]
+        if paren
+          if opts[:single_child]
+            Moperator.new(parent, token_hash[:lparen]) if token_hash[:lparen]
+            MRow.create_token(parent, token_hash[:e], :single_child => true)
+            Moperator.new(parent, token_hash[:rparen]) if token_hash[:rparen]
+          else
+            #TODO
+            # create new MRow
+            MRow.new(parent) do
+              Moperator.new(self, token_hash[:lparen]) if token_hash[:lparen]
+              MRow.create_token(self, token_hash[:e], :single_child => true)
+              Moperator.new(self, token_hash[:rparen]) if token_hash[:rparen]
+            end
+          end
+        else
+          MRow.create_token(parent, token_hash[:e])
+        end
+      when :unary
+        operator = token_hash[:operator]
+        tag("m#{operator}") do
+          MRow.create_token(parent, token_hash[:s], :single_child => true, :strip_paren => true)
+        end
+      when :binary
+        operator = token_hash[:operator]
+        tag("m#{operator}") do
+          MRow.create_token(parent, token_hash[:s1], :strip_paren => (operator != :sub && operator != :sup))
+          MRow.create_token(parent, token_hash[:s2], :strip_paren => true)
+        end
+      when :ternary
+        operator = token_hash[:operator]
+        tag("m#{operator}") do
+          MRow.create_token(parent, token_hash[:s1])
+          MRow.create_token(parent, token_hash[:s2], :strip_paren => true)
+          MRow.create_token(parent, token_hash[:s3], :strip_paren => true)
+        end
+      when :matrix
+        #TODO
+        # MRow.new(parent) do
+        #   Moperator.new(self, token_hash[:lparen]) if token_hash[:lparen]
+        #   MTable.new(self) do
+        #     token_hash[:rows].each do |row|
+        #       MTableRow do
+        #         row.each do |col|
+        #           mtd do
+        #             append(col)
+        #           end
+        #         end
+        #       end
+        #     end
+        #   end
+        #   Moperator.new(parent, token_hash[:rparen]) if token_hash[:rparen]
+        # end
+
+
+    end
+
+  end
+
+  class MToken < Container
+    attr_accessor :token_hash
+
+    def initialize(parent_graphic, options={})
+      super
+      @token_hash = options[:token_hash]
+
+      update_width
+      update_height
+      self
+    end
+
+    def update_width
+
+    end
+
+    def update_height
+
+    end
+  end
+
+  class MIdentifier < MToken
+    def initialize(parent_graphic, options={})
+      super
+      self
+    end
+  end
+
+  class MOperator
+    def initialize(parent_graphic, options={})
+      super
+      self
+    end
+  end
+
+  class MRoot < MToken
+    attr_accessor :operator
+    def initialize(parent_graphic, options={})
+      super
+      self
+    end
+
+  end
+
+  class MSqrt < MToken
+
+
+  end
+
+
+  class MFrac < MToken
+    attr_accessor :nominator, :denominator
+    def initialize(parent_graphic, options={})
+      super
+      self
+    end
+
+  end
+
+  class MSup < MToken
+    attr_accessor :base, :sup
+
+  end
+
+  class MSub < MToken
+    attr_accessor :base, :sub
+
+  end
+
+  # sum_sub^sup, int_sub^sup
+  # MTernary is when we have tree componemts, main, subscript, and superscript
+  # sum, iit, limit
+  class MTernary < MToken
+    attr_accessor :main, :sup, :sub
+  end
+
+  class MSum
+  end
+  class MIntegral
+  end
+  class MLimit
+
+  end
+  class MMatix
+
+  end
+end
