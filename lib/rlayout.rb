5f8c5e6f48bb9d2c1390c4aefbfd8e37bfe8371d
mskim <mskimsid@gmail.com>
2019-04-08 16:26:13 +0900

    commnet un-used files in rlayout.rb

--- lib/rlayout.rb
+++ lib/rlayout.rb
@@ -25,9 +25,9 @@ else
   require 'erb'
   require 'base64'
   require 'json'
+  require 'mini_magick'
   require 'hexapdf'
   # require 'xml-simple'
-  require 'mini_magick'
 
   require File.dirname(__FILE__) + "/rlayout/version"
   require File.dirname(__FILE__) + '/rlayout/base/utility'
@@ -51,20 +51,6 @@ else
   require File.dirname(__FILE__) + '/rlayout/container/grid'
   require File.dirname(__FILE__) + '/rlayout/container/pgscript'
 
-  require File.dirname(__FILE__) + '/rlayout/text/announcement_text'
-  require File.dirname(__FILE__) + '/rlayout/text/heading'
-  require File.dirname(__FILE__) + '/rlayout/text/heading_container'
-  require File.dirname(__FILE__) + '/rlayout/text/paragraph'
-  require File.dirname(__FILE__) + '/rlayout/text/quote_text'
-  require File.dirname(__FILE__) + '/rlayout/text/title_text'
-  require File.dirname(__FILE__) + "/rlayout/text/line_fragment"
-  require File.dirname(__FILE__) + "/rlayout/text/text"
-  require File.dirname(__FILE__) + "/rlayout/text/text_token"
-  require File.dirname(__FILE__) + '/rlayout/text/font'
-  require File.dirname(__FILE__) + '/rlayout/text/text_train'
-  require File.dirname(__FILE__) + '/rlayout/text/ordered_list'
-  require File.dirname(__FILE__) + '/rlayout/text/text_column'
-  require File.dirname(__FILE__) + '/rlayout/text/text_box'
 
   require File.dirname(__FILE__) + '/rlayout/table/simple_table'
   require File.dirname(__FILE__) + '/rlayout/table/table'
@@ -74,27 +60,9 @@ else
   require File.dirname(__FILE__) + '/rlayout/container_extended/place_item'
   require File.dirname(__FILE__) + '/rlayout/container_extended/memo_area'
 
-  require File.dirname(__FILE__) + "/rlayout/svg/canvas"
-  require File.dirname(__FILE__) + "/rlayout/svg/circle"
-  require File.dirname(__FILE__) + "/rlayout/svg/image"
-  require File.dirname(__FILE__) + "/rlayout/svg/line"
-  require File.dirname(__FILE__) + "/rlayout/svg/path"
-  require File.dirname(__FILE__) + "/rlayout/svg/polygon"
-  require File.dirname(__FILE__) + "/rlayout/svg/rectangle"
-  require File.dirname(__FILE__) + "/rlayout/svg/text"
-  require File.dirname(__FILE__) + "/rlayout/svg/svg2pdf"
-
   require File.dirname(__FILE__) + '/rlayout/style/style_service'
   require File.dirname(__FILE__) + '/rlayout/style/grid_layout'
   require File.dirname(__FILE__) + '/rlayout/style/image_layout'
-
-  require File.dirname(__FILE__) + '/rlayout/math/eqn'
-  require File.dirname(__FILE__) + '/rlayout/math/math_token'
-  require File.dirname(__FILE__) + '/rlayout/math/latex_math'
-
-  require File.dirname(__FILE__) + '/rlayout/page/page_fixtures'
-  require File.dirname(__FILE__) + '/rlayout/page/composite_page'
-  require File.dirname(__FILE__) + '/rlayout/page/page_variables_extend'
 
   require File.dirname(__FILE__) + '/rlayout/view/svg_view/document_view_svg'
   require File.dirname(__FILE__) + '/rlayout/view/svg_view/paragraph_view_svg'
@@ -114,15 +82,6 @@ else
   require File.dirname(__FILE__) + '/rlayout/story/story'
   require File.dirname(__FILE__) + '/rlayout/story/reader'
   require File.dirname(__FILE__) + '/rlayout/story/remote_reader'
-
-  require File.dirname(__FILE__) + '/rlayout/article/chapter'
-  require File.dirname(__FILE__) + '/rlayout/article/pdf_chapter'
-  require File.dirname(__FILE__) + '/rlayout/article/spread_chapter'
-  require File.dirname(__FILE__) + '/rlayout/article/toc_chapter'
-  require File.dirname(__FILE__) + '/rlayout/article/float_group'
-  require File.dirname(__FILE__) + '/rlayout/article/single_page_maker'
-  require File.dirname(__FILE__) + '/rlayout/article/rjob'
-  require File.dirname(__FILE__) + '/rlayout/article/item_chapter_maker'
 
   require File.dirname(__FILE__) + '/rlayout/chapter/r_text_token'
   require File.dirname(__FILE__) + '/rlayout/chapter/r_paragraph'
@@ -156,30 +115,72 @@ else
   require File.dirname(__FILE__) + '/rlayout/newspaper/news_section_page'
   require File.dirname(__FILE__) + '/rlayout/newspaper/news_make_tasks'
 
-  require File.dirname(__FILE__) + '/rlayout/magazine/magazine_article_maker'
+  # require File.dirname(__FILE__) + "/rlayout/svg/canvas"
+  # require File.dirname(__FILE__) + "/rlayout/svg/circle"
+  # require File.dirname(__FILE__) + "/rlayout/svg/image"
+  # require File.dirname(__FILE__) + "/rlayout/svg/line"
+  # require File.dirname(__FILE__) + "/rlayout/svg/path"
+  # require File.dirname(__FILE__) + "/rlayout/svg/polygon"
+  # require File.dirname(__FILE__) + "/rlayout/svg/rectangle"
+  # require File.dirname(__FILE__) + "/rlayout/svg/text"
+  # require File.dirname(__FILE__) + "/rlayout/svg/svg2pdf"
 
-  require File.dirname(__FILE__) + '/rlayout/publication/book'
-  require File.dirname(__FILE__) + '/rlayout/publication/book_plan'
-  require File.dirname(__FILE__) + '/rlayout/publication/dorok'
-  require File.dirname(__FILE__) + '/rlayout/publication/catalog'
-  require File.dirname(__FILE__) + '/rlayout/publication/composite'
-  require File.dirname(__FILE__) + '/rlayout/publication/magazine'
-  require File.dirname(__FILE__) + '/rlayout/publication/mart'
-  require File.dirname(__FILE__) + '/rlayout/publication/newspaper'
-  require File.dirname(__FILE__) + '/rlayout/publication/news_plan'
-  require File.dirname(__FILE__) + '/rlayout/publication/photo_book'
-  require File.dirname(__FILE__) + '/rlayout/publication/calendar'
-  require File.dirname(__FILE__) + '/rlayout/publication/marathon_tag'
-  require File.dirname(__FILE__) + '/rlayout/publication/member_directory'
-  require File.dirname(__FILE__) + '/rlayout/publication/banner'
+  # require File.dirname(__FILE__) + '/rlayout/math/eqn'
+  # require File.dirname(__FILE__) + '/rlayout/math/math_token'
+  # require File.dirname(__FILE__) + '/rlayout/math/latex_math'
 
-  require File.dirname(__FILE__) + '/rlayout/quiz/english_quiz_item'
-  require File.dirname(__FILE__) + '/rlayout/quiz/english_quiz_item_maker'
-  require File.dirname(__FILE__) + '/rlayout/quiz/quiz_chapter_maker'
-  require File.dirname(__FILE__) + '/rlayout/quiz/nr_semi_test_chapter_maker'
-  require File.dirname(__FILE__) + '/rlayout/math/mlayout/mdocument'
+  # require File.dirname(__FILE__) + '/rlayout/page/page_fixtures'
+  # require File.dirname(__FILE__) + '/rlayout/page/composite_page'
+  # require File.dirname(__FILE__) + '/rlayout/page/page_variables_extend'
+
+  # require File.dirname(__FILE__) + '/rlayout/text/announcement_text'
+  # require File.dirname(__FILE__) + '/rlayout/text/heading'
+  # require File.dirname(__FILE__) + '/rlayout/text/heading_container'
+  # require File.dirname(__FILE__) + '/rlayout/text/paragraph'
+  # require File.dirname(__FILE__) + '/rlayout/text/quote_text'
+  # require File.dirname(__FILE__) + '/rlayout/text/title_text'
+  # require File.dirname(__FILE__) + "/rlayout/text/line_fragment"
+  # require File.dirname(__FILE__) + "/rlayout/text/text"
+  # require File.dirname(__FILE__) + "/rlayout/text/text_token"
+  # require File.dirname(__FILE__) + '/rlayout/text/font'
+  # require File.dirname(__FILE__) + '/rlayout/text/text_train'
+  # require File.dirname(__FILE__) + '/rlayout/text/ordered_list'
+  # require File.dirname(__FILE__) + '/rlayout/text/text_column'
+  # require File.dirname(__FILE__) + '/rlayout/text/text_box'
+  # require File.dirname(__FILE__) + '/rlayout/article/chapter'
+  # require File.dirname(__FILE__) + '/rlayout/article/pdf_chapter'
+  # require File.dirname(__FILE__) + '/rlayout/article/spread_chapter'
+  # require File.dirname(__FILE__) + '/rlayout/article/toc_chapter'
+  # require File.dirname(__FILE__) + '/rlayout/article/float_group'
+  # require File.dirname(__FILE__) + '/rlayout/article/single_page_maker'
+  # require File.dirname(__FILE__) + '/rlayout/article/rjob'
+  # require File.dirname(__FILE__) + '/rlayout/article/item_chapter_maker'
+
+  # require File.dirname(__FILE__) + '/rlayout/magazine/magazine_article_maker'
+
+  # require File.dirname(__FILE__) + '/rlayout/publication/book'
+  # require File.dirname(__FILE__) + '/rlayout/publication/book_plan'
+  # require File.dirname(__FILE__) + '/rlayout/publication/dorok'
+  # require File.dirname(__FILE__) + '/rlayout/publication/catalog'
+  # require File.dirname(__FILE__) + '/rlayout/publication/composite'
+  # require File.dirname(__FILE__) + '/rlayout/publication/magazine'
+  # require File.dirname(__FILE__) + '/rlayout/publication/mart'
+  # require File.dirname(__FILE__) + '/rlayout/publication/newspaper'
+  # require File.dirname(__FILE__) + '/rlayout/publication/news_plan'
+  # require File.dirname(__FILE__) + '/rlayout/publication/photo_book'
+  # require File.dirname(__FILE__) + '/rlayout/publication/calendar'
+  # require File.dirname(__FILE__) + '/rlayout/publication/marathon_tag'
+  # require File.dirname(__FILE__) + '/rlayout/publication/member_directory'
+  # require File.dirname(__FILE__) + '/rlayout/publication/banner'
+
+  # require File.dirname(__FILE__) + '/rlayout/quiz/english_quiz_item'
+  # require File.dirname(__FILE__) + '/rlayout/quiz/english_quiz_item_maker'
+  # require File.dirname(__FILE__) + '/rlayout/quiz/quiz_chapter_maker'
+  # require File.dirname(__FILE__) + '/rlayout/quiz/nr_semi_test_chapter_maker'
+  # require File.dirname(__FILE__) + '/rlayout/math/mlayout/mdocument'
 end
 
 module RLayout
 
 end
+
