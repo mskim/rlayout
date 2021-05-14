a6ffe756e9781f5621211f90a4620566461fada5
Min Soo Kim <mskimsid@gmail.com>
2021-05-14 10:41:51 +0900

    add convertpdf2jpg to RDocument

--- lib/rlayout/view/pdf_view/r_document_view_pdf.rb
+++ lib/rlayout/view/pdf_view/r_document_view_pdf.rb
@@ -38,6 +38,8 @@ module RLayout
           page_folder_path = folder_path + "/#{page_folder_name}"
           FileUtils.mkdir_p(page_folder_path) unless File.exist?(page_folder_path)
           system("cd #{folder_path} && mv #{page_pdf_basename} #{page_folder_name}/page.pdf")
+          single_page_pdf_path = folder_path + "/#{page_folder_name}/page.pdf"
+          convert_pdf2jpg(single_page_pdf_path)
         end
       end
     end
@@ -49,6 +51,39 @@ module RLayout
         pdf_doc.fonts.add(font_file)
       end
     end
+
+
+    # using vips convert pdf 2 jpg
+    # if enlarging ratio is given as options, it enlarges pdf canvas to the given ratio, 
+    # then generates jpg, giving the hi-resolution result
+    def convert_pdf2jpg(output_path, options={})
+      @pdf_folder = File.dirname(output_path)
+      if options[:ratio]
+        ratio = options[:ratio] || 2.0
+        enlarged_path = output_path.gsub(/.pdf$/, "_enlarged.pdf")
+        target = HexaPDF::Document.new
+        src =  HexaPDF::Document.open(output_path)
+        src.pages.each do |page|
+          form = target.import(page.to_form_xobject)
+          width = form.box.width * ratio
+          height = form.box.height * ratio
+          canvas = target.pages.add([0, 0, width, height]).canvas
+          canvas.xobject(form, at: [0, 0], width: width, height: height)
+        end
+        target.write(enlarged_path, optimize: true)
+        @enlarged_pdf_base_name = File.basename(enlarged_path)
+        @jpg_base_name = File.basename(output_path).gsub(/.pdf$/, ".jpg")
+        commend  = "cd #{@pdf_folder} && vips copy #{@enlarged_pdf_base_name}[n=-1] #{@jpg_base_name}"
+        system(commend)
+        system("rm #{enlarged_path}")
+      else
+        @pdf_base_name = File.basename(output_path)
+        @jpg_base_name = @pdf_base_name.gsub(/.pdf$/, ".jpg")
+        commend  = "cd #{@pdf_folder} && vips copy #{@pdf_base_name}[n=-1] #{@jpg_base_name}"
+        system(commend)
+      end
+    end
+  
   end
 
 end
