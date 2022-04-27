7d02b7e3711aae3fed9e4c05cc1c4b0f2ccc55d1
Min Soo Kim <mskimsid@gmail.com>
2022-04-27 15:28:59 +0900

    add gap between overlap and text_area in line

--- lib/rlayout/document/r_line_fragment.rb
+++ lib/rlayout/document/r_line_fragment.rb
@@ -151,14 +151,16 @@ module RLayout
 
     # given a line_rect and float_rect, return un-cover rect
     def un_covered_rect(line_rect, covering_rect)
+      gap = 8
       if line_rect[0] >= covering_rect[0] && (line_rect[0] + line_rect[2]) <= (covering_rect[0] + covering_rect[2])
         # puts "covered all"
         [line_rect[0], line_rect[1] , 0, line_rect[3] ]
       elsif line_rect[0] < covering_rect[0]
         # puts "covered right side"
-        [line_rect[0], line_rect[1] , covering_rect[0] - line_rect[0], line_rect[3] ]
+        [line_rect[0], line_rect[1] , covering_rect[0] - line_rect[0] + gap, line_rect[3] ]
       elsif covering_rect[0] + covering_rect[2] <= (line_rect[0] + line_rect[2])
-        [covering_rect[0] + covering_rect[2], line_rect[1] , line_rect[0] + line_rect[2] - (covering_rect[0] + covering_rect[2]), line_rect[3] ]
+        # puts "covered left side
+        [covering_rect[0] + covering_rect[2] + gap, line_rect[1] , line_rect[0] + line_rect[2] - (covering_rect[0] + covering_rect[2] + gap), line_rect[3] ]
       else
         line_rect
       end
