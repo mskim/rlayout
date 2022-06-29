d5a876668ef895168b38432590c64a42e686d29a
Min Soo Kim <mskimsid@gmail.com>
2022-06-29 17:40:14 +0900

    change toc to inlcude styleable from subclassing

--- lib/rlayout/book/toc.rb
+++ lib/rlayout/book/toc.rb
@@ -7,24 +7,32 @@
 # some_text ............ 12
 
 module RLayout
-  class Toc < StyleableDoc
+  class Toc
     attr_reader :document_path
     attr_reader :document, :output_path, :column_count
     attr_reader :toc_content, :toc_title
     attr_reader :title, :paper_size, :page_count
     attr_reader :link_info, :max_page, :no_table_title
     attr_reader :toc_item_count, :paper_size, :custom_style
-
+    attr_reader :document_path
+    attr_reader :width, :height, :left_margin, :top_margin, :right_margin, :bottom_margin
+    # attr_reader :document
+    include Styleable
     def initialize(options={})
+      @document_path = options[:document_path]
+      @style_guide_folder = options[:style_guide_folder] || @document_path
+      @page_count = options[:page_count]
       @max_page = options[:max_page] || 1
-      super
       @toc_item_count = options[:toc_item_count] || 20
       @output_path    = options[:output_path] || @document_path + "/toc.pdf"
       @no_table_title = options[:no_table_title]
+      @page_pdf = options[:page_pdf] || true
+      @jpg = options[:jpg] || false
+      load_style
       read_toc
       layout_toc
       # @link_info = update_link_info
-      @document.save_pdf(@output_path, page_pdf:@page_pdf) unless options[:no_output]
+      @document.save_pdf(@output_path, page_pdf:@page_pdf, jpg: @jpg) unless options[:no_output]
       self
     end
 
@@ -66,10 +74,25 @@ module RLayout
         h[:layout_expand] = [:height]
         first_page.add_toc_title(h)
       end
-      @toc_content.each_slice(@toc_item_count).each_with_index do |page_data, i|
-        if @document.pages.length < i + 1
+
+      if @page_count 
+        page_item_count = (@toc_content.length / @page_count.to_f).ceil
+        @toc_content_for_page = @toc_content.each_slice(page_item_count).to_a
+      elsif @toc_item_count
+        #TODO: how about first page with title, it should be less than the rest???
+        @toc_content_for_page = @toc_content.each_slice(@toc_item_count).to_a
+      else
+        @toc_content_for_page = @toc_content.each_slice(20).to_a
+      end
+
+      if @document.pages.length < @toc_content_for_page.length
+        needed_page_count = @toc_content_for_page - @document.pages.length
+        needed_page_count.times do
           @document.add_new_page
         end
+      end
+
+      @toc_content_for_page.each_with_index do |page_data, i|
         page = @document.pages[i]
         h                 = {}
         h[:parent]        = page
@@ -117,6 +140,16 @@ module RLayout
       end
       layout =<<~EOF
         RLayout::RDocument.new(width:#{@width}, height:#{@height} , page_count: #{@max_page}, page_type: "toc_page")
+      EOF
+    end
+
+    def default_header_footer_yml
+      <<~EOF
+      ---
+      has_hearder: false
+      has_footer: false
+      ---
+
       EOF
     end
   end
